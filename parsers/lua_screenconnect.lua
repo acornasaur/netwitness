83a88a179e43599d37ca8e902aa79105d5ea2d5c
acornasaur <acornasaur@gmail.com>
2019-04-24 16:12:10 -0400

    Add files via upload

--- parsers/lua_screenconnect.lua
+++ parsers/lua_screenconnect.lua
@@ -30,12 +30,18 @@ local lua_screenconnect = nw.createParser("lua_screenconnect", "Identify screenc
         
 --]]
 
+lua_screenconnect:setKeys({
+    nwlanguagekey.create("alias.host",nwtypes.Text),
+    nwlanguagekey.create("alias.ip",nwtypes.IPv4),
+})
 
-
+local nwll = require('nwll')
 
 function lua_screenconnect:sessionBegin()
 	-- reset parser_state for the new session
 	self.state = nil
+	host = nil
+	key = nil
 end
 
 function lua_screenconnect:tokenMATCH(token, first, last)
@@ -59,10 +65,48 @@ function lua_screenconnect:tokenMATCH(token, first, last)
 				nw.logFailure(error)
 			end		
 		end	
-	end			
+	end	
+	
+	current_position = last + 1
+	local payload = nw.getPayload(current_position, current_position + 64)
+	if payload then
+		local tag1 = payload:uint8(1,1)
+		if tag1 == 4 then
+			--nw.logInfo("*** TAG 4 ***")
+			local start = 7
+			local num_temp = payload:find("\1", start + 1, -1)
+			if num_temp then
+				--nw.logInfo("*** 4: FOUND NUM_TEMP ***")
+				local myhost = payload:tostring(start, num_temp -1)
+				if myhost then
+					--nw.logInfo("*** 4 MYHOST: " .. myhost .. " ***")
+					host, key = nwll.determineHostType(myhost)
+					if host and key then
+						nw.createMeta(self.keys[key], host)
+					end
+				end
+			end
+		end
+		if tag1 == 6 then
+			--nw.logInfo("*** TAG 6 ***")
+			local start = 9
+			local num_temp = payload:find("\2", start + 1, -1)
+			if num_temp then
+				--nw.logInfo("*** 6: FOUND NUM_TEMP ***")
+				local myhost = payload:tostring(start, num_temp -1)
+				if myhost then
+					--nw.logInfo("*** 6 MYHOST: " .. myhost .. " ***")
+					host, key = nwll.determineHostType(myhost)
+					if host and key then
+						nw.createMeta(self.keys[key], host)
+					end
+				end
+			end
+		end
+	end	
 end
 
 lua_screenconnect:setCallbacks({
 	[nwevents.OnSessionBegin] = lua_screenconnect.sessionBegin,
-	["\115\16\0\0\0\0\0\0\0\0\0\0\0\0\0\0"] = lua_screenconnect.tokenMATCH, -- 73 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	["\115\16\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"] = lua_screenconnect.tokenMATCH, -- 73 10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
 })
