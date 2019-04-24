648cdb4038bf55846a6daf20edc3465d2e664464
acornasaur <acornasaur@gmail.com>
2019-04-24 12:08:10 -0400

    Add files via upload

---
+++ parsers/normalize_webpage.lua
@@ -0,0 +1,39 @@
+local normalize_webpage = nw.createParser("normalize_webpage", "Normalize webpage components")
+
+local nwll = require('nwll')
+
+normalize_webpage:setKeys({
+    nwlanguagekey.create("alias.host"),
+    nwlanguagekey.create("alias.ip"),
+    nwlanguagekey.create("directory"),
+    nwlanguagekey.create("filename"),
+    nwlanguagekey.create("extension"),
+    nwlanguagekey.create("query"),
+})
+ 
+function normalize_webpage:normalize_webpageMeta(index, meta)
+    local host, directory, filename, extension, querystring = nwll.extractUrlElements(meta)
+    if host then
+        local key
+        host, key = nwll.determineHostType(host)
+        if host and key then
+            nw.createMeta(self.keys[key], host)
+        end
+    end
+    if directory then
+        nw.createMeta(self.keys.directory, directory)
+    end
+    if filename then
+        nw.createMeta(self.keys.filename, filename)
+    end
+    if extension then
+        nw.createMeta(self.keys.extension, extension)
+    end
+    if querystring then
+        nw.createMeta(self.keys.query, querystring)
+    end
+end
+
+normalize_webpage:setCallbacks({
+    [nwlanguagekey.create("web.page")] = normalize_webpage.normalize_webpageMeta,
+})
